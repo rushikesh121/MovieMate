226b87d9515fe8d19a460ee5732c11c9f59eccd1
rushikesh121 <rautrushi1213@gmail.com>
2024-06-16 17:46:34 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../public/index.css";
-
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import MovieInfo from "../components/MovieInfo"
-import SearchInfo from "../components/SearchInfo";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Show from "../components/Show";
-import Body from "../components/Body";
-import About from "../components/About";
+import SearchInfo from "../components/SearchInfo"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Show from "../components/Show"
+import Body from "../components/Body"
+import About from "../components/About"
 export default function App() {
   return (
     <>
